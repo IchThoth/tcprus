92000158c889d9074b6d80b22b7b434ac9b512c3
ichthoth <shalomtata334@gmail.com>
2023-05-22 01:40:09 +0100

    accept and read connections

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net"
 )
 
@@ -30,6 +32,37 @@ func (s *Server) StartServer() error {
 	return nil
 }
 
-func main() {
+func (s *Server) AcceptLoop() {
+	for {
+		conn, err := s.listen.Accept()
+		if err != nil {
+			fmt.Println("accept error:", err)
+			continue
+		}
+		// each time there is a new connecton a new go routine is spun up to have a
+		//large number of non blocking connections
+		go s.ReadLoop(conn)
+	}
+}
 
+func (s *Server) ReadLoop(conn net.Conn) {
+	//switch size of bytes depending on size of message being sent
+	defer conn.Close()
+	buf := make([]byte, 2048)
+	for {
+		cn, err := conn.Read(buf)
+		if err != nil {
+			fmt.Println("read error:", err)
+			continue
+		}
+		//stringify message sent by client to tcp server
+		msg := buf[:cn]
+		fmt.Println(string(msg))
+	}
+
+}
+
+func main() {
+	server := NewServer(":6000")
+	log.Fatal(server.StartServer())
 }
